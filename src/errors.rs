1bd569cf63f6973c882bba54a672be85653e181b
guillermo-st <guillermo_stancanelli@outlook.com>
2023-05-26 13:01:15 -0300

    Create possible application errors

--- src/errors.rs
+++ src/errors.rs
@@ -3,3 +3,10 @@ pub enum CoffeeMakerError {
     /// Ocurrio un error en la lectura del archivo, ya sea porque no existe o tiene un formato equivocado
     FileReaderError,
 }
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum ServerError {
+    AccountNotFound,
+    NotEnoughPoints,
+    ConnectionLost,
+}
