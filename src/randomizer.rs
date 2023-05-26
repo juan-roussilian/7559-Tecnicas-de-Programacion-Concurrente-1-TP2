f1a8e2f258f1d7ed952758f98274292331127a73
guillermo-st <guillermo_stancanelli@outlook.com>
2023-05-26 13:00:40 -0300

    Create a helper randomizer for coffee success chance

---
+++ src/randomizer.rs
@@ -0,0 +1,22 @@
+use rand::Rng;
+
+pub trait Randomizer {
+    fn get_random_success(&self) -> bool;
+}
+
+pub struct RealRandomizer {
+    success_chance: i32,
+}
+
+impl RealRandomizer {
+    pub fn new(success_chance: int) -> Self {
+        Self { success_chance }
+    }
+}
+
+impl Randomizer for RealRandomizer {
+    fn get_random_success(&self) -> bool {
+        let num = rand::thread_rng().gen_range(0..100);
+        num >= self.success_chance
+    }
+}
