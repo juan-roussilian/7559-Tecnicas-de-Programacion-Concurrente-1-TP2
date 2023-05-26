516126a0994fc2f18aac43d460b0c52c4d0238d3
guillermo-st <guillermo_stancanelli@outlook.com>
2023-05-26 13:04:44 -0300

    Separate messages to their own file

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,10 @@ use coffee_maker::main_coffee;
 
 pub mod coffee_maker;
 pub mod errors;
+pub mod messages;
+pub mod order;
+pub mod orders_reader;
+pub mod randomizer;
+pub mod server;
 
 fn main() {}
---
+++ src/messages.rs
@@ -0,0 +1,23 @@
+use crate::coffee_maker::CoffeeMaker;
+use crate::order::Order;
+use actix::Addr;
+
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct OpenFile(pub Addr<CoffeeMaker>);
+
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct ReadAnOrder;
+
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct ProcessOrder(pub Order);
+
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct OpenedFile;
+
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct ErrorOpeningFile;
