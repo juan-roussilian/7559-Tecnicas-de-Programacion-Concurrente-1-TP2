4844a6193eb837cf7b685d5bb49fb293b21adcbd
Bruno Grassano <bgrassano@fi.uba.ar>
2023-06-06 19:30:15 -0300

    Create ServerMessage
    close #22
    
    Co-Authored-By: Juan Roussilian <58794566+juan-roussilian@users.noreply.github.com>
    Co-Authored-By: Guillermo Stancanelli <66892180+guillermo-st@users.noreply.github.com>

--- src/server/server_messages.rs
+++ src/server/server_messages.rs
@@ -1,14 +1,37 @@
-use serde::{Deserialize, Serialize};
+use std::{ time::{ Instant, Duration }, collections::{ HashMap, HashSet } };
+
+use chrono::{ DateTime, Local };
+use lib::local_connection_messages::CoffeeMakerRequest;
+use serde::{ Deserialize, Serialize };
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct AccountChange {
-    pub account_id: usize,
-    pub points: usize,
+pub struct ServerMessage {
+    pub message_type: ServerMessageType,
+    pub sender_id: usize,
+    pub passed_by: HashSet<usize>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub enum ServerMessage {
-    // TODO
-    AddPoints(AccountChange),
-    TakePoints(AccountChange),
+pub enum ServerMessageType {
+    NewConnection(Diff),
+    CloseConnection,
+    Token(TokenData),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TokenData {
+    changes: HashMap<usize, Vec<CoffeeMakerRequest>>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Diff {
+    last_update: Duration,
+    changes: Vec<UpdatedAccount>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UpdatedAccount {
+    pub id: usize,
+    pub amount: usize,
+    pub last_updated_on: Duration,
 }
