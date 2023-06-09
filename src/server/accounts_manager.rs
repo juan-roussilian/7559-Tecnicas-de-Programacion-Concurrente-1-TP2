8b4463ef9259bf48066893aae8e4067e04d06f52
Juan <jroussilian@fi.uba.ar>
2023-06-09 13:21:15 -0300

    Change accounts_manager  interface and adapt in memory accounts manager

--- src/server/accounts_manager.rs
+++ src/server/accounts_manager.rs
@@ -2,9 +2,8 @@ use crate::errors::ServerError;
 
 pub trait AccountsManager {
     fn new(&self) -> Self;
-    fn add_points(&mut self, account_id: usize, points: usize);
+    fn add_points(&mut self, account_id: usize, points: usize, operation_time: Option<u128>) -> Result<(), ServerError>;
+    fn substract_points(&self, account_id: usize, points: usize,operation_time: Option<u128>) -> Result<(), ServerError>;
     fn request_points(&self, account_id: usize, points: usize) -> Result<(), ServerError>;
     fn cancel_requested_points(&self, account_id: usize) -> Result<(), ServerError>;
-    fn substract_points(&self, account_id: usize, points: usize) -> Result<(), ServerError>;
-    fn update_account(&self, account_id: usize, points: usize) -> Result<(), ServerError>;
 }
--- src/server/memory_accounts_manager.rs
+++ src/server/memory_accounts_manager.rs
@@ -11,13 +11,14 @@ pub struct MemoryAccountsManager {
 }
 
 impl AccountsManager for MemoryAccountsManager {
+
     fn new(&self) -> Self {
         MemoryAccountsManager {
             accounts: HashMap::new(),
         }
     }
 
-    fn add_points(&mut self, account_id: usize, points: usize) {
+    fn add_points(&mut self, account_id: usize, points: usize, operation_time: Option<u128>)->Result<(),ServerError> {
         if self.accounts.contains_key(&account_id) {
             if let Some(account) = self.accounts.get(&account_id) {
                 if let Ok(account_guard) = account.write() {}
@@ -28,6 +29,7 @@ impl AccountsManager for MemoryAccountsManager {
                     .insert(account_id, Arc::new(RwLock::new(new_account)));
             }
         }
+        Ok(())
     }
 
     fn request_points(&self, account_id: usize, points: usize) -> Result<(), ServerError> {
@@ -36,10 +38,7 @@ impl AccountsManager for MemoryAccountsManager {
     fn cancel_requested_points(&self, account_id: usize) -> Result<(), ServerError> {
         Ok(())
     }
-    fn substract_points(&self, account_id: usize, points: usize) -> Result<(), ServerError> {
-        Ok(())
-    }
-    fn update_account(&self, account_id: usize, points: usize) -> Result<(), ServerError> {
+    fn substract_points(&self, account_id: usize, points: usize, operation_time: Option<u128>) -> Result<(), ServerError> {
         Ok(())
     }
 }
