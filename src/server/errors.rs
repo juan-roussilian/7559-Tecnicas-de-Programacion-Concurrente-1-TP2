73cfa14fe8e4157253efe0abb011a9b319a5231a
Juan <jroussilian@fi.uba.ar>
2023-06-09 17:49:29 -0300

    Add CoffeServerStart error
    Co-authored-by: Bruno Grassano <bgrassano@fi.uba.ar>

--- src/server/errors.rs
+++ src/server/errors.rs
@@ -14,6 +14,7 @@ pub enum ServerError {
     SerializationError,
     OperationIsOutdated,
     AccountIsReserved,
+    CoffeeServerStartError
 }
 
 impl<T> From<std::sync::PoisonError<T>> for ServerError {
