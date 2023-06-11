2b1fa9d801d418477b97c0bd963e703f1d049dc3
Bruno Grassano <bgrassano@fi.uba.ar>
2023-06-10 21:32:14 -0300

    Increase connection channel timeout

--- src/server/constants.rs
+++ src/server/constants.rs
@@ -6,7 +6,7 @@ pub const MAX_WAIT_IN_MS_FOR_CONNECTION_ATTEMPT: u64 = 3600000;
 
 /// Es el tiempo de timeout que tiene el sender hacia la next connection.
 /// Si no recibe nada en este tiempo revisa si esta conectado. Si lo esta se toma como que hay demora.
-pub const TO_NEXT_CONN_CHANNEL_TIMEOUT_IN_MS: u64 = 5000;
+pub const TO_NEXT_CONN_CHANNEL_TIMEOUT_IN_MS: u64 = 22000;
 
 /// Indica el tiempo que se espera a recibr el resultado de un cafe con puntos. Debe de ser por lo menos algo mas que lo que toma hacer un cafe.
 /// Ver la constante PROCESS_ORDER_TIME_IN_MS en la cafetera
